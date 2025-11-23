48a7e3161b1fc2af22454fc2cf997952be08fb5b
CHr15P4C <cspacitt@gmail.com>
2025-11-23 17:43:55 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+Clicker-Game
+
+General Idea:
+A game similar to custom maps where you can practice your mouse clicking accuracy. 
+
+General Structure:
+A section that holds targets to be clicked on. 
+    Goals:
+        targets appearing sequentially 1 at a time
+        targets appearing in groups, groups appreaing sequentially
+        Difficulty: incorporating different sizes for different difficulties.
+        Variation: random shapes and colours
+        Make the grid auto fit!!!!!
+
+A section with game controls and score.
+    Start, pause, clear, score.
+    Goals:
+        ammount of targets input. 
+        dropdown for style.
+        color control.
+
+Design decisions:
+generate all targets at beginning and manipulate visibility based on actions, or generate random
+targets as they get clicked on
+
---
+++ script.js
@@ -0,0 +1,71 @@
+let targetNum=16;
+let sqrtTargetNum=Math.sqrt(targetNum);
+
+//Buttons
+const startButton = document.getElementById("start-btn");
+const stopButton = document.getElementById("stop-btn");
+
+//page elements
+const targetGrid = document.getElementById("target-grid");
+
+
+//eventListeners
+
+startButton.addEventListener('click', () => {
+    generateTargets();
+});
+
+
+//FUNCTIONS
+
+//generate all targets
+
+function generateTargets() {
+    //clear the grid
+    targetGrid.innerHTML = '';
+    //generate the number of targets based on the set ammount
+    for (i=0;i<targetNum;i++) {
+        
+        targetGrid.innerHTML += `<div id="${i}" class="${i} target  ">target${i}</div>`;
+    }
+    //show one target
+    showTarget(1);
+    //bind event listeners to each active target.
+    bindTarget();
+
+
+}
+
+//bing target click
+function bindTarget() {
+     document.querySelectorAll('.target.active').forEach(btn=> {
+        btn.addEventListener('click', () => {
+            console.log('clicked');
+            displayNextTarget();
+
+        });
+    });
+}
+
+
+//show target
+
+function showTarget(targetNum) {
+    document.querySelectorAll('.target').forEach(targ => {
+            targ.classList.remove('active');
+            console.log('first part of show target')
+    });
+    document.getElementById(targetNum).classList.add('active');
+    console.log('second part of show section')
+}
+
+//show random target
+function displayNextTarget() {
+    showTarget(getRandomInt(targetNum))
+    bindTarget();
+}
+
+//random int generator
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
