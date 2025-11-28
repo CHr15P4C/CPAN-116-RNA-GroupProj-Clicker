a0f74b76c67824dc72847028e28689e648782ee9
CHr15P4C <cspacitt@gmail.com>
2025-11-28 12:53:26 -0500

    adding randomized colour functionality for targets

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//code variables and constants.
 let targetNum=200;
 let sqrtTargetNum=parseInt(Math.sqrt(targetNum));
 let targetsMax=200;
+let colourVars = ['Aqua', 'Blue', 'BlueViolet', 'Brown', 'Chartreuse', 'Black', 'Coral', 'Crimson'];
+
 //Buttons
 const startButton = document.getElementById("start-btn");
 const stopButton = document.getElementById("stop-btn");
@@ -40,7 +43,7 @@ function generateTargets() {
 
 }
 
-//bing target click
+//bind target click
 function bindTarget() {
      document.querySelectorAll('.target.active').forEach(btn=> {
         btn.addEventListener('click', () => {
@@ -57,18 +60,43 @@ function bindTarget() {
 //show target
 
 function showTarget(targetNum) {
+    //clear all targets colors
+    clearColors();
+    //clear all targets
     document.querySelectorAll('.target').forEach(targ => {
             targ.classList.remove('active');
             console.log('first part of show target')
     });
+    //update 1 target class to active
     document.getElementById(targetNum).classList.add('active');
+    //set random color for targetsquare
+    setTargetRandomColor(targetNum);
     console.log('second part of show section')
+
 }
 
+
+//setting a targets color randomly
+function setTargetRandomColor(targetNum) {
+    let numberColours = colourVars.length;
+    let colourNumber = getRandomInt(numberColours);
+    //update 1 target color
+    document.getElementById(targetNum).style["background-color"] = `${colourVars[colourNumber]}`
+}
+
+//clearing all targets color:
+
+function clearColors() {
+    document.querySelectorAll('.target').forEach(targ => {
+            targ.style["background-color"] = 'white';
+            console.log('targets cleared')
+    });
+}
+    
 //show random target
 function displayNextTarget() {
     showTarget(getRandomInt(targetNum))
-    bindTarget();
+    bindTarget();   
 }
 
 //random int generator
@@ -76,27 +104,40 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-//style injection from java to html
+//style injection from java to html - unnecessary?
+/*
 function addStyle(styleString) {
   const style = document.createElement('style');
   style.textContent = styleString;
   document.head.append(style);
 }
+*/
 
 //setting grid rows
 function setGrid() {
+    //setting grid using a css variable, editing root.
+    /*
     addStyle(`
         :root {
             --column-var: ${sqrtTargetNum};
             }
         `);
-}
+        */
+       //setting variable by editing innerHTML
+    document.getElementById("target-grid").style["grid-template-columns"] = `repeat(${sqrtTargetNum}, 1fr)`;
+    console.log("generated new grid rows:" + sqrtTargetNum);
+    }
+
+
+
+
 
 //update target variables
 
 function updateTargetVars() {
     targetNum=getRandomInt(targetsMax);
-    sqrtTargetNum=parseInt(Math.sqrt(targetNum));
+    //trying to get nice balance of total targets to number used for # of columns in the grid.
+    sqrtTargetNum=parseInt(Math.sqrt(targetNum)+4);
     console.log(targetNum);
     console.log(sqrtTargetNum);
 }
